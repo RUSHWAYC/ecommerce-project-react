081ac8627a73a967dd1ce42010bb2f727bd1e225
Rushwayc <rushwayc@yahoo.com>
2022-11-11 12:40:50 +0100

    Homepage and product pages completed.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Head from 'next/head'
 
-const Layout = () => {
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Elecomm sound</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      {/* Gets children, or all files from _app that is
+          getting them from index.
+          _app will be loaded on every page, and as such
+          header and footer will be loaded as well.*/}
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
---
+++ pages/product/[slug].js
@@ -0,0 +1,128 @@
+import { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutline, Star, AiOutlineStar} from 'react-icons/ai'
+import { Product } from '../../components'
+
+import { client, urlFor } from '../../lib/client'
+
+const ProductDetails = ({ product, products }) => {
+
+    const { image, name, details, price } = product
+    const [index, setIndex] = useState(0)
+
+  return (
+    <div>
+       <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+                    <img src={urlFor(image && image[index])}
+                        className='product-detail-image'
+                    />
+                </div>
+                {/* Small images beneath the main image. */}
+                <div className='small-images-container'>
+                    {image?.map((item, i) => (
+                        <img
+                         src={urlFor(item)}
+                         className={i === index ? 'small-image selected-image' : 'small-image'}
+                         //onMouseEnter replace the mainimage with hovered one
+                         //and apply classes above.
+                         onMouseEnter={() => setIndex(i)}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className='product-detail-desc'>
+                <h1>{name}</h1>
+                <div className='reviews'>
+                    <div>
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
+                    </div>
+                    <p>(20)</p>
+                </div>
+                <h4>Details: </h4>
+                <p>{details}</p>
+                <p className='price'>${price}</p>
+                <div className='quantity'>
+                    <h3>Quanitity: </h3>
+                    <p className='quantity-desc'>
+                        <span className='minus'
+                        onClick=''><AiOutlineMinus /></span>
+                        <span className='num'
+                        onClick=''>0</span>
+                        <span className='plus'
+                        onClick=''><AiOutlinePlus /></span>
+                    </p>
+                </div>
+                <div className='buttons'>
+                    <button type='button'
+                            className='add-to-cart'
+                            onClick=''
+                    >
+                        Add to Cart
+                    </button>
+                    <button type='button'
+                            className='buy-now'
+                            onClick=''
+                    >
+                        Buy Now
+                    </button>
+                </div>
+            </div>
+        </div> 
+        <div className='maylike-products-wrapper'>
+            <h2>You may also like</h2>
+            <div className='marquee'>
+                <div className='maylike-products-container track'>
+                    {products.map((item) => (
+                        <Product key={item._id} product={item}/>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
+//If you enter a pre-loaded page that contains more pre-loaded
+//pages, gotta set up getStaticPaths for them.
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`
+    const products = await client.fetch(query)
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }))
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+
+//getStaticProps pre-load the pages when users enters the site.
+export const getStaticProps = async ({ params: { slug } }) => {
+    //Get the product info of the first product that matches the slug.
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`
+    //Get all products. Will be used for recommended items.
+    const productQuery = '*[_type == "product"]'
+    //Get the queried single product.
+    const product = await client.fetch(query);
+    //Get the queried all products.
+    const products = await client.fetch(productQuery);
+  
+    return {
+      props: { product, products }
+    }
+  }
+
+export default ProductDetails
