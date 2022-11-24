cdf493123cb5841fb929ffd19016313dc8eedfbf
Rushwayc <rushwayc@yahoo.com>
2022-11-24 23:33:17 +0100

    Started Stripe integration.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -12,6 +12,10 @@ const Cart = () => {
   const cartRef = useRef()
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext()
 
+  const handleCheckout = () => {
+    
+  }
+
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -91,7 +95,7 @@ const Cart = () => {
               </div>
               <div className='btn-container'>
                 <button type='button' className='btn'
-                  onClick=''>
+                  onClick={handleCheckout}>
                     Pay with Stripe
                 </button>
               </div>
